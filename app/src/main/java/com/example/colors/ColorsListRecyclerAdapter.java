04445a05d602e753e4238a9f452ceaa9e4f1a9f9
Vladislav Golodnyk <61089156+13h4i15@users.noreply.github.com>
2020-02-28 22:32:27 +0400

    Made focus listeners for list elems.

--- app/src/main/java/com/example/colors/ColorsListRecyclerAdapter.java
+++ app/src/main/java/com/example/colors/ColorsListRecyclerAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -24,6 +25,7 @@ class ColorsListRecyclerAdapter extends RecyclerView.Adapter<ColorsListRecyclerA
             textView = itemView.findViewById(R.id.colors_list_item_text);
             //image = itemView.findViewById(R.id.colors_list_item_image);
         }
+
     }
 
     public ColorsListRecyclerAdapter(ArrayList<ColorListElem> colorListElems) {
@@ -41,13 +43,28 @@ class ColorsListRecyclerAdapter extends RecyclerView.Adapter<ColorsListRecyclerA
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.color_list_item_layout, parent, false);
         context = parent.getContext();
         final RecyclerViewHolder recyclerViewHolder = new RecyclerViewHolder(view);
+        //view.setFocusableInTouchMode(true);
+        view.setOnClickListener( v -> {
+           /* LinearLayout layout = v.findViewById(R.id.colors_list_item_body);
+            layout.setBackgroundColor(ContextCompat.getColor(context, ColorListElem.ItemColorState.getColorByPosition(0)));*/
+        });/*
+        view.setOnFocusChangeListener( (v, isFocused) ->{
+           *//* if(isFocused){
+                LinearLayout layout = v.findViewById(R.id.colors_list_item_body);
+                layout.setBackgroundColor(ContextCompat.getColor(context, ColorListElem.ItemColorState.getColorByPosition(0)));
+            }
+            else {
+                LinearLayout layout = v.findViewById(R.id.colors_list_item_body);
+                layout.setBackgroundColor(ContextCompat.getColor(context, ColorListElem.ItemColorState.getColorByPosition(7)));
+            }*//*
+        });*/
         return recyclerViewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerViewHolder holder, int position) {
         holder.textView.setText("Item" + position);//todo
-        holder.textView.setTextColor(ContextCompat.getColor(context, ColorListElem.ItemColorState.getColorByPosition(position)));
+       // holder.textView.setTextColor(ContextCompat.getColor(context, ColorListElem.ItemColorState.getColorByPosition(position)));
         //holder.image.setBackgroundColor(colorListElems.get(position).getColor());
     }
 
