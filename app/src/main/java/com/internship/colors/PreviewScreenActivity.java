7cb05f57062c723bf19d443dab2d8665b801714a
Vladislav Golodnyk <61089156+13h4i15@users.noreply.github.com>
2020-03-05 14:53:36 +0400

    Added handlerPauseAction that starts new thread with pause and send message for handler.

--- app/src/main/java/com/internship/colors/PreviewScreenActivity.java
+++ app/src/main/java/com/internship/colors/PreviewScreenActivity.java
@@ -1,14 +1,48 @@
 package com.internship.colors;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.Toast;
 
-public class PreviewScreenActivity extends AppCompatActivity {
+import java.util.concurrent.TimeUnit;
 
+
+public class PreviewScreenActivity extends AppCompatActivity{
+    Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preview_screen);
+        Intent mainActivityIntent = new Intent(this, MainActivity.class);
+        handler = new Handler(){
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                startActivity(mainActivityIntent);
+                finish();
+            }
+        };
+        pauseAction();
+    }
+
+
+    private void pauseAction(){
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    TimeUnit.SECONDS.sleep(2);
+                    handler.sendEmptyMessage(0);
+                }catch (InterruptedException e){
+
+                }
+            }
+        });
+        thread.start();
     }
 }
