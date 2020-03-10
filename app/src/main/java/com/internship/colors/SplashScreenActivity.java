4b65208784917868d390c3675753458476eddb1d
Vladislav Golodnyk <61089156+13h4i15@users.noreply.github.com>
2020-03-10 13:55:08 +0400

    Edited time delay.

--- app/src/main/java/com/internship/colors/SplashScreenActivity.java
+++ app/src/main/java/com/internship/colors/SplashScreenActivity.java
@@ -23,7 +23,7 @@ public class SplashScreenActivity extends AppCompatActivity {
 
         if (savedInstanceState == null) { //it doesn't work if thread was initially activated
             CompositeDisposable disposable = new CompositeDisposable();
-            disposable.add(Observable.timer(5000, TimeUnit.MILLISECONDS)
+            disposable.add(Observable.timer(2000, TimeUnit.MILLISECONDS)
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribe(v -> {
