9614f874d3cc2395394bbaaa5ff47521f47c6623
Vladislav Golodnyk <61089156+13h4i15@users.noreply.github.com>
2020-03-10 20:22:51 +0400

    Added state listeners to improve activity loading. Now if you close app, it terminates timer action, or if you hide app and the timer is over, the application will start new activity only after you open it again.

--- app/src/main/java/com/internship/colors/SplashScreenActivity.java
+++ app/src/main/java/com/internship/colors/SplashScreenActivity.java
@@ -1,34 +1,86 @@
 package com.internship.colors;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 
+import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class SplashScreenActivity extends AppCompatActivity {
+    private final String START_TIME_TAG = "millis";
+
+    private long millisFromStart;
+    private CompositeDisposable compositeDisposable;
+    private Disposable disposable;
+    private boolean visibilityFlag = true;
+    private boolean delayEndedFlag = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        if (savedInstanceState == null) { //it doesn't work if thread was initially activated
-            CompositeDisposable disposable = new CompositeDisposable();
-            disposable.add(Observable.timer(2000, TimeUnit.MILLISECONDS)
+        if (savedInstanceState != null) {
+            millisFromStart = savedInstanceState.getLong(START_TIME_TAG);
+        } else {
+            millisFromStart = Calendar.getInstance().getTimeInMillis();
+        }
+
+        long currentTime = Calendar.getInstance().getTimeInMillis();
+        long millisOut = 2000 - (currentTime - millisFromStart);
+
+        if (millisOut > 0) {
+            compositeDisposable = new CompositeDisposable();
+            disposable = Observable.timer(millisOut, TimeUnit.MILLISECONDS)
                     .subscribeOn(Schedulers.computation())
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribe(v -> {
-                        Intent mainActivityIntent = new Intent(this, MainActivity.class);
-                        startActivity(mainActivityIntent);
-                        finish();
-                    }));
+                        if (visibilityFlag) startMainActivity();
+                        delayEndedFlag = true;
+                    });
+            compositeDisposable.add(disposable);
+        } else {
+            startMainActivity();
         }
+    }
+
+    private void startMainActivity() {
+        Intent mainActivityIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+        startActivity(mainActivityIntent);
+        finish();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putLong(START_TIME_TAG, millisFromStart);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (compositeDisposable != null) compositeDisposable.remove(disposable);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        visibilityFlag = false;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        visibilityFlag = true;
+        if (delayEndedFlag) startMainActivity();
     }
 }
