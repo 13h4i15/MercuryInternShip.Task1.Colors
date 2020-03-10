5cb0f6fc4c83680b37689819de3b7ea933c4d2c6
Vladislav Golodnyk <61089156+13h4i15@users.noreply.github.com>
2020-03-10 13:50:48 +0400

    Just some tabulations.

--- app/src/main/java/com/internship/colors/SplashScreenActivity.java
+++ app/src/main/java/com/internship/colors/SplashScreenActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
