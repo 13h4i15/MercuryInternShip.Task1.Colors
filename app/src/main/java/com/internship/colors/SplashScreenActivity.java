4478a05a2c3addd9de0d24336d7ae95e5c300c34
Vladislav Golodnyk <61089156+13h4i15@users.noreply.github.com>
2020-03-11 18:45:37 +0400

    Removed useless flags, now it disposes dispose object on pause and restart it on resume. Renamed two variables. Tag String now static.

--- app/src/main/java/com/internship/colors/SplashScreenActivity.java
+++ app/src/main/java/com/internship/colors/SplashScreenActivity.java
@@ -9,19 +9,16 @@ import android.os.Bundle;
 import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
 
-import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.core.Single;
-import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class SplashScreenActivity extends AppCompatActivity {
-    private final String START_TIME_TAG = "millis";
+    private final static String START_TIME_TAG = "millis";
 
-    private long millisFromStart;
-    private CompositeDisposable compositeDisposable;
-    private boolean visibilityFlag = true;
+    private long millisAtStart;
+    private Disposable disposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,56 +26,47 @@ public class SplashScreenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
 
         if (savedInstanceState != null) {
-            millisFromStart = savedInstanceState.getLong(START_TIME_TAG);
+            millisAtStart = savedInstanceState.getLong(START_TIME_TAG);
         } else {
-            millisFromStart = Calendar.getInstance().getTimeInMillis();
-        }
-
-        long currentTime = Calendar.getInstance().getTimeInMillis();
-        long millisOut = 2000 - (currentTime - millisFromStart);
-
-        if (millisOut > 0) {
-            compositeDisposable = new CompositeDisposable();
-            Disposable disposable = Single.timer(millisOut, TimeUnit.MILLISECONDS)
-                    .subscribeOn(Schedulers.computation())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(v -> {
-                        if (visibilityFlag) startMainActivity();
-                    });
-            compositeDisposable.add(disposable);
-        } else {
-            startMainActivity();
+            millisAtStart = Calendar.getInstance().getTimeInMillis();
         }
     }
 
     private void startMainActivity() {
-        Intent mainActivityIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+        Intent mainActivityIntent = new Intent(this, MainActivity.class);
         startActivity(mainActivityIntent);
         finish();
     }
 
     @Override
     public void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putLong(START_TIME_TAG, millisAtStart);
+
         super.onSaveInstanceState(outState);
-        outState.putLong(START_TIME_TAG, millisFromStart);
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (compositeDisposable != null) compositeDisposable.dispose();
-    }
 
     @Override
     protected void onPause() {
+        if (disposable != null) disposable.dispose();
+
         super.onPause();
-        visibilityFlag = false;
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        visibilityFlag = true;
-        if (compositeDisposable.isDisposed()) startMainActivity();
+    protected void onResume() {
+        super.onResume();
+
+        long currentTime = Calendar.getInstance().getTimeInMillis();
+        long millisOut = 2000 - (currentTime - millisAtStart);
+
+        if (millisOut > 0) {
+            disposable = Single.timer(millisOut, TimeUnit.MILLISECONDS)
+                    .subscribeOn(Schedulers.computation())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(v -> startMainActivity());
+        } else {
+            startMainActivity();
+        }
     }
 }
